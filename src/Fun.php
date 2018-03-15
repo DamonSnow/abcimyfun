1a93b7debe37fabd73491619d84921691d6a2437
DamonSnow <domine_huang@unicompound.com>
2018-03-15 16:28:46 +0800

    ffff

---
+++ src/Fun.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Damon
+ * Date: 2018/3/15
+ * Time: 16:14
+ */
+
+namespace Damon\Fun;
+
+
+class Fun
+{
+    public function print($test)
+    {
+        echo $test;
+    }
+}
